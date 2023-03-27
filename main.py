f9f65cf10fcf7c9e30c09c5fb7d612eb2c6041a8
IReames24 <818123@mystma.org>
2023-03-27 14:56:23 -0500

    3

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 def greaterThan(x, y):
     if x > y:
-        return("True")
+        return True
     else:
-        return("False")
+        return False
 def lessThan(x, y):
     if x < y:
-        return("True")
+        return True
     else:
-        return("False")
+        return False
 def equalTo(x, y):
     if x == y:
-        return("True")
+        return True
     else:
-        return("False")
+        return False
 def greaterOrEqual(x, y):
     if x >= y:
-        return("True")
+        return True
     else:
-        return("False")
+        return False
 def lessOrEqual(x, y):
     if x <= y:
-        return("True")
+        return True
     else:
-        return("False")
+        return False
